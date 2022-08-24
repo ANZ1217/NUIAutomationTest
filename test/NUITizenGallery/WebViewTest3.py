410f4d6a27c9a2f94a34dcb522a8d64aa94e0eaa
huayong.xu <huayong.xu@samsung.com>
2022-08-24 18:23:57 +0800

    Add aurum TCs for webview & widgetview.

---
+++ test/NUITizenGallery/WebViewTest3.py
@@ -0,0 +1,122 @@
+# test python script must be in same location as aurum_pb2.py
+# Link of tested app is https://github.com/Samsung/TizenFX/tree/master/test/Tizen.NUI.WebViewTest
+
+from aurum_pb2 import *
+from aurum_pb2_grpc import BootstrapStub
+from NUIGalleryTestUtils import *
+import grpc
+import time
+
+isWebViewOpened = False
+
+# Check if WebView page is opened or not.
+def CheckWebViewTestStart(stub):
+    if not LaunchAppTest(stub):
+        return False
+
+    global isWebViewOpened
+    isWebViewOpened = FindTCByInputText(stub, "WebViewTest3")
+    time.sleep(2)
+    return isWebViewOpened
+
+
+# Check green key.
+def CheckWebViewTest11(stub):
+    if not isWebViewOpened:
+        return False
+
+    # Move focus to WebView.
+    for i in range(2):
+        stub.sendKey(ReqKey(type='XF86', actionType='STROKE', XF86keyCode='Down'))
+        time.sleep(0.3)
+
+    stub.sendKey(ReqKey(type='XF86', actionType='STROKE', XF86keyCode='XF86Green'))
+    time.sleep(2)
+
+    # Take screen shot.
+    screenShort = ReadScreenShotFile(stub, fileName="WebView/WebViewTest31.png")
+    if screenShort is None:
+        return False
+
+    # Read image file expected.
+    expectedScreenShot = ReadImageFile(fileName='WebView/WebViewTestExpected31.png')
+    if expectedScreenShot is None:
+        return False
+
+    # Check ssim.
+    return CheckSSIM(answerImge=expectedScreenShot, testTargetImage=screenShort)
+
+
+# Check yellow key.
+def CheckWebViewTest12(stub):
+    if not isWebViewOpened:
+        return False
+
+    stub.sendKey(ReqKey(type='XF86', actionType='STROKE', XF86keyCode='XF86Yellow'))
+    time.sleep(2)
+
+    # Take screen shot.
+    screenShort = ReadScreenShotFile(stub, fileName="WebView/WebViewTest32.png")
+    if screenShort is None:
+        return False
+
+    # Read image file expected.
+    expectedScreenShot = ReadImageFile(fileName='WebView/WebViewTestExpected32.png')
+    if expectedScreenShot is None:
+        return False
+
+    # Check ssim.
+    return CheckSSIM(answerImge=expectedScreenShot, testTargetImage=screenShort)
+
+
+# Check blue key.
+def CheckWebViewTest13(stub):
+    if not isWebViewOpened:
+        return False
+
+    for i in range(3):
+        stub.sendKey(ReqKey(type='XF86', actionType='STROKE', XF86keyCode='XF86Blue'))
+        time.sleep(2)
+
+    # Take screen shot.
+    screenShort = ReadScreenShotFile(stub, fileName="WebView/WebViewTest33.png")
+    if screenShort is None:
+        return False
+
+    # Read image file expected.
+    expectedScreenShot = ReadImageFile(fileName='WebView/WebViewTestExpected33.png')
+    if expectedScreenShot is None:
+        return False
+
+    # Check ssim.
+    return CheckSSIM(answerImge=expectedScreenShot, testTargetImage=screenShort)
+
+
+# Check if WebView exit normally.
+def CheckWebViewTestEnd(stub):
+    # Exit Gallery.
+    for i in range(2):
+        stub.sendKey(ReqKey(type='XF86', actionType='STROKE', XF86keyCode='XF86Exit'))
+        time.sleep(1)
+    return True
+
+
+def runTest(stub, testFunc):
+    print("Testing started :", testFunc)
+    result = testFunc(stub)
+    print("Testing result :", result)
+    return True
+
+
+def run():                                                         
+    with grpc.insecure_channel('localhost:50051') as channel:      
+        stub = BootstrapStub(channel)
+        runTest(stub, CheckWebViewTestStart)
+        runTest(stub, CheckWebViewTest11)
+        runTest(stub, CheckWebViewTest12)
+        runTest(stub, CheckWebViewTest13)
+        runTest(stub, CheckWebViewTestEnd)
+
+
+if __name__ == '__main__':                                         
+    run()
---
+++ test/NUITizenGallery/WidgetViewTest1.py
@@ -0,0 +1,68 @@
+# test python script must be in same location as aurum_pb2.py
+
+from aurum_pb2 import *
+from aurum_pb2_grpc import BootstrapStub
+from NUIGalleryTestUtils import *
+import grpc
+import time
+
+isWidgetViewOpened = False
+
+# Launch application. it returns application running state
+def CheckWidgetViewTestStart(stub):
+    if not LaunchAppTest(stub):
+        return False
+
+    global isWidgetViewOpened
+    isWidgetViewOpened = FindTCByInputText(stub, "WidgetViewTest1")
+    time.sleep(2)
+    return isWidgetViewOpened
+
+
+# Check '1' key.
+def CheckWidgetViewTest11(stub):
+    if not isWidgetViewOpened:
+        return False
+
+    stub.sendKey(ReqKey(type='XF86', actionType='STROKE', XF86keyCode='1'))
+    time.sleep(1)
+
+    # Take screen shot.
+    screenShort = ReadScreenShotFile(stub, fileName="WidgetView/WidgetViewTest11.png")
+    if screenShort is None:
+        return False
+
+    # Read image file expected.
+    expectedScreenShot = ReadImageFile(fileName='WidgetView/WidgetViewTestExpected11.png')
+    if expectedScreenShot is None:
+        return False
+
+    # Check ssim.
+    return CheckSSIM(answerImge=expectedScreenShot, testTargetImage=screenShort)
+
+
+# Check if WidgetView exit normally.
+def CheckWidgetViewTestEnd(stub):
+    # Exit.
+    stub.sendKey(ReqKey(type='XF86', actionType='STROKE', XF86keyCode='XF86Exit'))
+    time.sleep(1)
+    return True
+
+
+def runTest(stub, testFunc):
+    print("Testing started :", testFunc)
+    result = testFunc(stub)
+    print("Testing result :", result)
+    return True
+
+
+def run():                                                         
+    with grpc.insecure_channel('localhost:50051') as channel:      
+        stub = BootstrapStub(channel)
+        runTest(stub, CheckWidgetViewTestStart)
+        runTest(stub, CheckWidgetViewTest11)
+        runTest(stub, CheckWidgetViewTestEnd)
+
+
+if __name__ == '__main__':                                         
+    run()
