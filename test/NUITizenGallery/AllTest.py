9db83d3f7217f4216b95d7da49b0ebab887330f4
EverLEEst(SangHyeon Lee) <sh10233.lee@samsung.com>
2022-09-20 20:55:00 +0900

    update Report html

--- test/NUITizenGallery/AllTest.py
+++ test/NUITizenGallery/AllTest.py
@@ -49,22 +49,24 @@ def RunAllTest(ret):
     failedCount = 0
     failedTest = ""
 
-    f.write("<head><title>NUITizenGallery All Test {}</title></head>\n".format(time.strftime('%Y-%m-%d %H:%M')))
-    f.write("<body>\n<table border=1>\n<th>Test</th>\n<th>Result</th>\n");
+    testDate = time.strftime('%Y-%m-%d %H:%M')
+    f.write("<head><title>NUITizenGallery All Test {}</title></head>\n".format(testDate))
+    f.write("<body>\n<h1>NUITizenGallery All Test {}</h1>\n".format(testDate))
+    f.write("<table border=1>\n<th>Test</th>\n<th>Result</th>\n");
 
     tcFileNameList = GetTCList()
     for item in tcFileNameList:
-        itemTitle = "====================[ {} ]=======================".format(item)
+        itemTitle = "=============================[ {} ]=============================".format(item)
         print("\033[47m\033[30m" + itemTitle + "\033[0m")
 
         fcount = RunTest(item, ret)
         if fcount == 0:
             passedCount+=1
-            ret.write("<tr>\n<td>" + item + "</td>\n<td><span style=\"color: #006600\">Pass</span></td>\n</tr>\n")
+            ret.write("<tr>\n<td width=500>" + item + "</td>\n<td width=150 style=\"text-aglign: center\"><span style=\"color: #006600\">Pass</span></td>\n</tr>\n")
         else:
             failedCount+=1
             failedTest+=item+" "
-            ret.write("<tr>\n<td>" + item + "</td>\n<td><span style=\"color: #CC0000\">Failed</span></td>\n</tr>\n")
+            ret.write("<tr>\n<td width=500>" + item + "</td>\n<td width=150 style=\"text-aglign: center\"><span style=\"color: #CC0000\">Failed</span></td>\n</tr>\n")
 
     f.write("</table>\n")
 
@@ -72,11 +74,11 @@ def RunAllTest(ret):
     tcCount = "Total TCs: {}".format(len(tcFileNameList))
     pCount = "Passed: {}".format(passedCount)
     fCount = "Failed: {}".format(failedCount)
-    resultTitle = "====================[ {} ]=======================".format(tcCount)
+    resultTitle = "=============================[ {} ]=============================".format(tcCount)
     print("\033[48;5;33m\033[38;5;235m" + resultTitle + "\033[0m")
     ret.write("<p><b>" + resultTitle+ "<br>\n")
     print("\033[92m" + pCount + ", \033[91m" + fCount + "\033[0m")
-    ret.write("<span style=\"color: #006600\">Pass</span>" + pCount + ", <\span><span style=\"color: #CC0000\">" + fCount + "<span><br>\n")
+    ret.write("<span style=\"color: #006600\">" + pCount + "<\span>, <span style=\"color: #CC0000\">" + fCount + "<span><br>\n")
     if failedCount != 0:
         failedList = "Failed Test : [{}]".format(failedTest)
         print("\033[38;5;214m" + failedList + "\033[0m")
